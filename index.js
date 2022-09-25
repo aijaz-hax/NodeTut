58731dc2e6e6b7bec39d84ad53a098b19e2ee4a4
Aijaz.js <89201608+aijaz-hax@users.noreply.github.com>
2022-09-25 10:36:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// console.log("My Name is Aijax"); My First line for node.
+//    console.error("Ijax");
+
+////////////////////////////////////
+// var x=10;
+// var y=20;
+// var z= x+y;
+// console.log("The addition is ",z); My Second line for node.
+ 
+////////////////////////////////////
+
+// import {x} from "./index1" Not workin o o 
+// console.log(x);
+
+// const app=require("./index1");
+// console.log(app.z());
+
+/////////////////////////////////////
+
+// const arr=[9,9,8,6,5,4]     FILTER METHOD Interview Question
+// const myNewArray=arr.filter((val)=>{
+//     return val > 5;
+// })
+// console.log("My New Array is ",myNewArray);
+
+////////////////////////////////////
+// Make basic server
+const color=require("colors");
+console.log("I like Green".bgGreen);
+const http=require("http");
+http.createServer((req,resp)=>{
+    resp.write("I am Aijax");
+    resp.end();
+}).listen(4500);
---
+++ index1.js
@@ -0,0 +1,9 @@
+// export var x="Hello i am importing you OK!";
+
+module.exports ={
+    x:"Hello i am importing you OK!",
+    y:"Hello i know import function not working!",
+    z:function(){
+        return "Aijax Ahamed Bhat";
+    }
+}
